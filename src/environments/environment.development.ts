81faa08f2c37a95692c7395b34913efb42d58239
JoelHuanca <u20201b914@upc.edu.pe.com>
2024-09-25 22:06:59 -0500

    first commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
   // Server Base Path for Fake REST API
-  serverBasePath: 'http://localhost:3000'
+  //serverBasePath: 'http://localhost:3000'
+  serverBasePath: 'https://my-json-server.typicode.com//InspiraSalud/db-server-CristhianHuanca'
   // Server Base Path for Spring Boot REST API
   // serverBasePath: 'http://localhost:8090/api/v1'
 };
